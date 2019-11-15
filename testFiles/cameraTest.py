1c1ad20c9664dc2f4cbc4629e18fe1ef9cfcec74
Bram <big-b+git@wayfarer.org>
2019-11-15 12:46:18 -0500

    cup'

--- testFiles/cameraTest.py
+++ testFiles/cameraTest.py
@@ -2,6 +2,5 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-sleep(3)
-camera.capture('/home/pi/project3/ENGR195_project3_team10/classfier/image.jpg')
+camera.capture('/home/pi/project3/ENGR195_project3_team10/classifier/image.jpg')
 
