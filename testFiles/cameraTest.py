db77840a71db3386a1ec3aff78afe2ce53025ae3
Bram <big-b+git@wayfarer.org>
2019-11-20 12:57:16 -0500

    fabric?

--- testFiles/cameraTest.py
+++ testFiles/cameraTest.py
@@ -1,5 +1,5 @@
 from picamera import PiCamera
-from time import sleep
+#from time import sleep
 
 camera = PiCamera()
 camera.capture('/home/pi/project3/ENGR195_project3_team10/classifier/image.jpg')
