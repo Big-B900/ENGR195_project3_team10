40ecee3f2f15ee2d068f3b5449aae3264322649e
Bram <big-b+git@wayfarer.org>
2019-11-21 00:05:18 -0500

    OH MY GOD IT WORKED

--- scripts/main.py
+++ scripts/main.py
@@ -10,13 +10,22 @@ BUTTON_1 = crickit.SIGNAL1
 ss.pin_mode(BUTTON_1,ss.INPUT_PULLUP)
 camera = PiCamera()
 c = Connection('kanga2')
+direction = 0
 
 while True:
     if not(ss.digital_read(BUTTON_1)):
         #print("yuh")
         fn.picToTop(camera)
         #testfn()
+        #make sure picture is recieved
         time.sleep(.5)
+        #this runs the neural network on the laptop and returns a dictionary of the confidences
         result = fn.vision(c)
-        print(result.stdout.strip())
-        fn.rotateThetrough(1)
+        
+        print(result)#.stdout.strip())
+       
+        #logic
+        #if result
+        
+        
+        fn.rotateThetrough(direction)
