17d9a86ac5de971a73586706b67facb17855849b
Bram <big-b+git@wayfarer.org>
2019-11-22 12:31:50 -0500

    "automated git push"

--- testFiles/laptopTest/autoGit.py
+++ testFiles/laptopTest/autoGit.py
@@ -16,7 +16,7 @@ print(p.communicate())
 print(q.communicate())
 print(r.communicate())
 #c.local("git pull")
-
+c.run("cd {} && git pull".format("/home/pi/project3/ENGR195_project3_team10/"))
 
 
 
