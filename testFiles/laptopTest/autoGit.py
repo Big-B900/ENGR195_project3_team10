ac3942f6116602a34d6e72c004475c902e8af0ab
Bram <big-b+git@wayfarer.org>
2019-11-22 12:29:49 -0500

    oh no

---
+++ testFiles/laptopTest/autoGit.py
@@ -0,0 +1,29 @@
+from fabric import task, Connection
+import subprocess
+
+c = Connection('raspi')
+
+p = subprocess.Popen(["git", "add","."], stdout=subprocess.PIPE)
+q = subprocess.Popen(["git", "commit","-m",'"automated git push"'], stdout=subprocess.PIPE)
+
+#c.local('git config --global user.email "big-b+git@wayfarer.org"')
+#c.local('git config --global user.name "Bram"')
+
+#c.local("git add .")
+#c.local("git commit -m 'automated git push'")
+#c.local("git push")
+print(p.communicate())
+print(q.communicate())
+c.local("git pull")
+
+
+
+
+
+#def autoGit(c):
+#	vision_dir = '/home/testuser/gitprojects/ENGR195_project3_team10/classifier/'
+#	env_dir = "/home/testuser/"
+#	result =  c.run("cd {} && source env/bin/activate && cd {} && python3 runvision.py".format(env_dir,vision_dir),hide=True)
+#	return result.stdout.strip()
+
+
